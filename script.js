0b7735bb8fa5270e2a064b018fe2ee1ee7af3bcf
rian23-TNI <markettr02@gmail.com>
2026-04-11 22:19:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ window.addEventListener("load", function () {
 
 
 function notif() {
-	alert('Not Function')
-	alert('thanks you!')
+	alert('"Not Function"')
+	alert('"thanks you!"')
 }
 var i = 0;
 var txt = 'hallo... saya Trianto. Saya seorang pengembang perusahaan yang berdedikasi dan bersemangat dalam pekerjaan. Saya memiliki pengalaman dalam mengembangkan projek work seperti usaha makanan, pelayanan perbaikan mesin maupun elektrikal engineering juga klien dari berbagai industri perkebunan termasuk kakek saya yang memiliki keahlian bidang pertanian dan pendidikan.untuk lebih lanjutnya bisa di tinjau di alamat dan kontak di bawah ini : ';
